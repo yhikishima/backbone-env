0c10be476b9f793ca7173e4271f9d15450a5cb96
yhikishima <lallapalooza1120@gmail.com>
2015-11-29 23:44:25 +0900

    Add Module

--- 07.MarionetteJs/dist/js/app.js
+++ 07.MarionetteJs/dist/js/app.js
@@ -28,7 +28,37 @@ var App = new Backbone.Marionette.Application();
 
 // App起動時に呼ばれる
 App.addInitializer(function () {
-  console.log('init');
+  var router = new Router();
+});
+
+var Router = Backbone.Router.extend({
+  routes: {
+    "": "index",
+    ":cotent": "changeContent"
+  },
+  index: function index() {
+    console.log('index');
+  },
+  changeContent: function changeContent() {
+    console.log('changeContent');
+  }
+});
+
+App.module('MyModule', function (MyModule) {
+  MyModule.Controller = Backbone.Marionette.Controller.extend({});
+
+  MyModule.addInitializer(function () {
+    // 例：モジュールの`start`に合わせてインスタンスを生成
+    console.log('start');
+    MyModule.Controller = new MyModule.Controller();
+  });
+
+  MyModule.addFinalizer(function () {
+    // 例：モジュールの`stop`に合わせてインスタンスを破棄
+    MyModule.Controller.close();
+    console.log('close');
+    delete MyModule.Controller;
+  });
 });
 
 // Appを起動
--- 07.MarionetteJs/src/js/app.js
+++ 07.MarionetteJs/src/js/app.js
@@ -15,7 +15,39 @@ let App = new Backbone.Marionette.Application();
 
 // App起動時に呼ばれる
 App.addInitializer(() => {
-  console.log('init');
+  var router = new Router();
+});
+
+let Router = Backbone.Router.extend({
+  routes: {
+    "": "index",
+    ":cotent": "changeContent"
+  },
+  index: () => {
+    console.log('index');
+  },
+  changeContent: () => {
+    console.log('changeContent');
+  }
+});
+
+
+App.module('MyModule', (MyModule) => {
+  MyModule.Controller = Backbone.Marionette.Controller.extend({
+  });
+
+  MyModule.addInitializer(() => {
+    // 例：モジュールの`start`に合わせてインスタンスを生成
+    console.log('start');
+    MyModule.Controller = new MyModule.Controller();
+  });
+
+  MyModule.addFinalizer(() => {
+    // 例：モジュールの`stop`に合わせてインスタンスを破棄
+    MyModule.Controller.close();
+    console.log('close');
+    delete MyModule.Controller;
+  });
 });
 
 // Appを起動
